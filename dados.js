e7ca4016aef74ce88ccbd9f9a9ff566b8aeded24
camila-OL <camila-OL>
2023-10-18 21:06:41 -0300

    file atualizado

--- dados.js
+++ dados.js
@@ -2,9 +2,12 @@ let dados = [{
     images: ['images/miaPlay.png', 'images/miaIntro.gif', 'images/miaFail.png',
             'images/robertaPlay.gif', 'images/robertaIntro.png', 'images/robertaFail.png',
             'images/lupIntro.png', 'images/lupPlay.gif', 'images/lupFail.png',
-            'images/RBD1.gif'],
+            'images/RBD1.gif',
+            'images/boysIntro.png', 'images/boysPlay.gif', 'images/boysFail.png',
+            'images/girlsIntro.png', 'images/Girls.gif', 'images/girlsFail.png',
+            'images/Banda.gif'],
     audios: ['audio/Aún Hay Algo.mp3', 'audio/Tras De Mí.mp3','audio/cuando.mp3', 'audio/RBD - Este Corazón (Lyric Video)(MP3_160K).mp3',
-            'audio/fail.mp3'],
+            'audio/fail.mp3', 'audio/futuroEx.mp3', 'audio/Ser O Parecer(MP3_160K).mp3'],
     listas: [['<button class="hay"> Aún Hay Algo </button>', 
               '<button class="error"> Así soy yo </button>',
               '<button class="error"> Nuestro amor </button>',
@@ -18,7 +21,17 @@ let dados = [{
             ['<button class="error"> Un poco de tu amor </button>', 
             '<button class="error"> Medley Rebelde </button>',
             '<button class="error"> No Pares </button>',
-            '<button class="cuan"> Cuando el amor se acaba </button>']]
+            '<button class="cuan"> Cuando el amor se acaba </button>'],
+        
+            ['<button class="error"> Inalcanzable </button>', 
+            '<button class="error"> Bésame Sin Miedo </button>',
+            '<button class="fut"> Futuro ex‐novio </button>',
+            '<button class="error"> Tenerte y quererte </button>'],
+        
+            ['<button class="error"> Ser ou parecer </button>', 
+            '<button class="error"> Santa no soy </button>',
+            '<button class="error"> A Tu Lado </button>',
+            '<button class="ser"> Ser O Parecer </button>']]
 }]
 
 
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ let cont = document.querySelector('.content')
 let exit = document.querySelector('.sair')
 let title = document.querySelector('.title')
 let areaT = document.querySelector('.area-total')
+let areai = document.querySelector('.area-int')
 
 
 start.addEventListener('click', () => {
@@ -137,47 +138,170 @@ start.addEventListener('click', () => {
                     })
                     //trocar lista 2
                     lst.innerHTML = armL[2]
-                    //parte 3 acerto
+
+                    //parte 4
                     let c = document.querySelector('.cuan')
                     c.addEventListener('click', () => {
-                        title.style.display = 'none'
-                        rein.style.display = 'block'
-                        exit.style.display = 'block'
-                        cont.style.width = '300px'
-                        let nam = document.querySelector('.name').value
-                        let namM = nam.toUpperCase()
-                        aud.parentNode.removeChild(aud)
-                        image.parentNode.removeChild(image)
-                        list.parentNode.removeChild(list)
-                        //texto
-                        text.innerHTML = `<span> PARABÉNS VOCÊ </br> É REALMENTE </br> FÃ ${namM} </span>`
-                        //trocar imagem 3
-                        imgTrue = document.createElement('img')
-                        imgTrue.src = arm[9]
-                        imgTrue.style.width = '400px'
-                        imgTrue.style.top = '73%'
-                        imgTrue.style.left = '6%'
-                        imgTrue.style.position = 'absolute'
-                        espImage.appendChild(imgTrue)
-                        //parte 3 audio
-                        let audEnd = new Audio(armA[3])
-                        audEnd.addEventListener('ended', function() {
-                            this.currentTime = 0
-                            this.play()
+                        rein.style.display = 'none'
+                        //trocar imagem 2
+                        image.src = arm[11]
+                        //trocar audio 2
+                        aud.setAttribute('src', armA[5])
+                        //trocar imagem 2 play 
+                        aud.addEventListener('play', () => {
+                            image.src = arm[11]
+                            image.style.width = '400px'
+                            image.style.top = '56%'
+                            image.style.right = '-15%'
                         })
-                        audEnd.play()
-                        //parte 3 reiniciar 
-                        rein.innerHTML = 'Reiniciar'
-                        exit.innerHTML = 'Sair'
-                        rein.style.cursor = 'pointer'
-                        exit.style.cursor = 'pointer'
-                        rein.addEventListener('click', () => {
-                            location.reload()
+                        aud.addEventListener('pause', () => {
+                            image.src = arm[10]
+                            image.style.top = '48%'
+                            image.style.width = '350px'
                         })
-                        exit.addEventListener('click', () => {
-                            window.close()
+                        //trocar lista 2
+                        lst.innerHTML = armL[3]
+                    
+                    //parte 4 erro
+                        let error = document.querySelectorAll('.error')
+                        error.forEach(element => {
+                        element.addEventListener('click', () => {
+                            rein.style.display = 'block'
+                            const fail = new Audio(armA[4])
+                            fail.play()
+
+                            aud.parentNode.removeChild(aud)
+                            image.parentNode.removeChild(image)
+                            list.parentNode.removeChild(list)
+
+                            //add imagem erro
+                            let imgErro = document.createElement('img')
+                            imgErro.src = arm[12]
+                            rein.style.marginBottom = '47%'
+                            rein.style.marginTop = '33%'
+                            cont.style.padding = '0'
+                            imgErro.style.width = '400px'
+                            espImage.appendChild(imgErro)
+                
+                            //reiniciar
+                            rein.innerHTML = 'Reiniciar'
+                            rein.style.cursor = 'pointer'
+                            rein.addEventListener('click', () => {
+                                location.reload()
+                            })
+
                         })
+                        })
+                        //parte 5
+                        let f = document.querySelector('.fut')
+                        f.addEventListener('click', () => {
+                            rein.style.display = 'none'
+                            //trocar imagem 2
+                            image.src = arm[14]
+                            //trocar audio 2
+                            aud.setAttribute('src', armA[6])
+                            //trocar imagem 2 play 
+                            aud.addEventListener('play', () => {
+                                image.src = arm[14]
+                                image.style.width = '430px'
+                                image.style.top = '60%'
+                                image.style.right = '-17%'
+                            })
+                            aud.addEventListener('pause', () => {
+                                image.src = arm[13]
+                                image.style.top = '55%'
+                                image.style.width = '400px'
+                            })
+                            //trocar lista 2
+                            lst.innerHTML = armL[4]
+
+                            //parte 5 acerto
+                            let s = document.querySelector('.ser') 
+                            s.addEventListener('click', () => {
+                                cont.style.backgroundImage = "url('images/back.jpg')"
+                                title.style.display = 'none'
+                                rein.style.display = 'block'
+                                rein.style.marginBottom = '0'
+                                exit.style.display = 'block'
+                                cont.style.width = '300px'
+                                let nam = document.querySelector('.name').value
+                                aud.parentNode.removeChild(aud)
+                                image.parentNode.removeChild(image)
+                                list.parentNode.removeChild(list)
+                                //texto
+                                text.innerHTML = `<div class="sign">
+                                <div class="neon-blue" id="title"> P<span id="fade"> arabéns </span> Você </div>
+                                <div class="neon-blue"> é <span class="neon-purple" id="trav"> Realmente </span> Fã <span class="neon-purple"> ${nam} </span></div>
+                                </div>`
+                                //trocar imagem 3
+                                imgTrue = document.createElement('img')
+                                imgTrue.src = arm[16]
+                                imgTrue.style.width = '400px'
+                                imgTrue.style.height = '250px'
+                                imgTrue.style.top = '63%'
+                                imgTrue.style.left = '6%'
+                                imgTrue.style.position = 'absolute'
+                                espImage.appendChild(imgTrue)
+                                //parte 3 audio
+                                let audEnd = new Audio(armA[3])
+                                audEnd.addEventListener('ended', function() {
+                                    this.currentTime = 0
+                                    this.play()
+                                })
+                                audEnd.play()
+                                //parte 3 reiniciar 
+                                rein.innerHTML = 'Reiniciar'
+                                exit.innerHTML = 'Sair'
+                                rein.style.cursor = 'pointer'
+                                exit.style.cursor = 'pointer'
+                                rein.addEventListener('click', () => {
+                                    location.reload()
+                                })
+                                exit.addEventListener('click', () => {
+                                    window.close()
+                                })
+                            })
+
+                            //parte 5 erro
+                            let error = document.querySelectorAll('.error')
+                            error.forEach(element => {
+                            element.addEventListener('click', () => {
+                            rein.style.display = 'block'
+                            const fail = new Audio(armA[4])
+                            fail.play()
+
+                            aud.parentNode.removeChild(aud)
+                            image.parentNode.removeChild(image)
+                            list.parentNode.removeChild(list)
+
+                            //add imagem erro
+                            let imgErro = document.createElement('img')
+                            imgErro.src = arm[15]
+                            rein.style.marginBottom = '40%'
+                            rein.style.marginTop = '25%'
+                            imgErro.style.width = '450px'
+                            cont.style.padding = '0'
+                            espImage.appendChild(imgErro)
+                
+                            //reiniciar
+                            rein.innerHTML = 'Reiniciar'
+                            rein.style.cursor = 'pointer'
+                            rein.addEventListener('click', () => {
+                                location.reload()
+                            })
+
+                        })
+
                     })
+                        })
+
+                        
+                        /*
+                        
+                    */
+                    })
+                    
+                    
                     //parte 3 erro
                     let error = document.querySelectorAll('.error')
                     error.forEach(element => {
@@ -206,8 +330,10 @@ start.addEventListener('click', () => {
 
                         })
 
-                    })     
-                    //
+                    })
+                    
+                   
+                    
                 })
 
         })
